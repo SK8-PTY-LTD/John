c6654ef0736375ecacf3de5481d608e780e18018
johnma01 <361804281@qq.com>
2016-08-25 13:06:01 +1000

    11111

--- js/app.js
+++ js/app.js
@@ -10,3 +10,35 @@ $(".navbar-brand button").click(function(){
 	$(this).remove();
 })
 
+//photo wall
+
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+
+//An image to overlay
+$overlay.append($image);
+
+//Add overlay
+$("body").append($overlay);
+  //A caption
+
+
+$("#photoWall img").click(function(event){
+  event.preventDefault();
+  var imageLocation = $(this).attr("src");
+  //Update overlay with the image linked in the link
+  $image.attr("src", imageLocation);
+  
+  //Show the overlay.
+  $overlay.show();
+  
+  //Get child's alt attribute and set caption
+
+
+});
+
+//When overlay is clicked
+$overlay.click(function(){
+  //Hide the overlay
+  $overlay.hide();
+});
